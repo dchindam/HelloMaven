043b9bec20c0d990dd00e1c35db979fc5f7bd4b6
DELL <DELL@10.14.209.45>
2017-09-05 11:43:21 -0400

    my first maven

---
+++ src/main/java/myfirstmaven/HelloMaven/App.java
@@ -0,0 +1,23 @@
+package myfirstmaven.HelloMaven;
+
+public class App 
+
+{
+   
+ public static void main( String[] args )
+  
+  {
+       
+ System.out.println( "Hello World!" );
+   
+ }
+
+	
+public int calculateSomething()
+ {
+
+return 0;
+	
+}
+
+}
